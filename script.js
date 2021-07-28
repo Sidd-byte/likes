8ac1b7e8b75f1f1097f5baa007f6fb77c8d44bc1
Sidd-byte <damonrobles4@gmail.com>
2021-07-28 08:04:48 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("hello wrld")
+var like_counter = document.querySelector("#change_1")
+var amount_likes = 9;
+function like(){
+    amount_likes++;
+    like_counter.innerText = amount_likes + " likes"
+}
+var like_counter1 = document.querySelector("#change_1")
+var amount_likes1 = 12;
+function like1(){
+    amount_likes1++;
+    like_counter1.innerText = amount_likes1 + " likes1"
+}
+var like_counter2 = document.querySelector("#change_3")
+var amount_likes2 = 90;
+function like2(){
+    amount_likes2++;
+    like_counter2.innerText = amount_likes2 + " likes2"
+}
